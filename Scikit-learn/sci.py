fdc1d9c669239311b773906b441fcd6ae78fcb1b
mehmadullahsheikh <mehmadullahsheikh@gmail.com>
2026-04-19 15:44:11 +0530

    scikit-learn done

--- Scikit-learn/sci.py
+++ Scikit-learn/sci.py
@@ -43,7 +43,113 @@ from sklearn.datasets import load_iris
 iris=load_iris()
 
 import pandas as pd
-X=pd.DataFrame(iris.data,columns=iris.feature_name)
+X=pd.DataFrame(iris.data,columns=iris.feature_names)
 y=pd.Series(iris.target)
 df=X.copy()
 df['target']=y
+# print(df.head())
+
+## visualizing dataset structure
+## class distribution
+import seaborn as sns
+import matplotlib.pyplot as plt
+# sns.countplot(x=iris.target)
+# plt.title("class distribution in iris")
+# plt.xlabel("Target class")
+# plt.ylabel("Count")
+# plt.show()
+
+## pairwise feature relations
+# sns.pairplot(df,hue='target',corner=True)
+# plt.suptitle("Pairplot of Iris Features",y=1.02)
+# plt.show()
+
+### correlation map
+# sns.heatmap(df.corr(),annot=True,cmap="coolwarm")
+# plt.title("Feature Correlation Haetmap")
+# plt.show()
+
+####### Data preprocessing
+# why preprocess
+# ml models expert numeric data 
+# features should be scaled for models KNN,SVM
+
+#### split the data
+from sklearn.model_selection import train_test_split
+
+X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.2,random_state=42)
+
+
+## scale the features
+from sklearn.preprocessing import StandardScaler
+
+scaler=StandardScaler()
+X_train=scaler.fit_transform(X_train)
+X_test=scaler.transform(X_test)
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+fig,ax=plt.subplots(1,2,figsize=(14,5))
+ax[0].hist(X.iloc[:,0],bins=20,color='skyblue')
+ax[0].set_title("Before")
+
+ax[1].hist(X_train[:,0],bins=20,color='orange')
+ax[1].set_title("after")
+plt.show()
+
+######## Ml models in scikit-learn #################
+
+########## k-nearest neighbours
+#   think of  it like this
+# you moved into a new neighborhood and you dont know which cricket team you should join so you look around at the 5 people closest to you
+# if 3 of them play for team a and 2 for team b you join team a
+# why? because most of your nnneighbor are  in team a
+
+# simple example 
+# you want to find out if a fruit is a apple or an orange
+# you check the 5 closest fruits in your dataset based on size and color
+# if 4 of them are apples and i is an orange-> the   n ew fruit is probably an apple
+
+########## knn implememtation #####
+from sklearn.neighbors import KNeighborsClassifier
+
+knn=KNeighborsClassifier(n_neighbors=3)
+knn.fit(X_train,y_train)
+y_pred_knn=knn.predict(X_test)
+
+######### decision trees ########
+# simple example : should you go outside
+#you are trying to decide whether to go outside or stay home
+#you ask yourself a few yes/no questions in order 
+# 1.is it raining 
+    # a: yes->stay home
+    # b: move to the next question
+# 2.do you have work to do
+#   # a:yes ->stay home
+#   # b:no ->go outside
+# this is exactly the decision into smaller questions . each questions  is a node. each answer leads to another branch.at the end you get a final decision    
+
+######## desicion treee
+from sklearn.tree import DecisionTreeClassifier
+
+tree=DecisionTreeClassifier(max_depth=3,random_state=42)
+tree.fit(X_train,y_train)
+y_pred_tree=tree.predict(X_test)
+
+###### visualizing model evaluation
+###### confusion matrix heatmap(for both models)
+from sklearn.metrics import confusion_matrix,classification_report
+def plot_confusion(y_true,y_pred,title):
+    cm=confusion_matrix(y_true,y_pred)
+    sns.heatmap(cm,annot=True,fmt='d',cmap="Blues")
+    plt.xlabel("Predicted")
+    plt.ylabel("Actual")
+    plt.title(title)
+    plt.show()
+
+plot_confusion(y_test,y_pred_knn,"KNN CONFUSION MATRIX")
+plot_confusion(y_test,y_pred_tree,"DECISION TREE CONFUSION MATRIX")   
+
+print("KNN Report:\n",classification_report(y_test,y_pred_knn))
+print("Tree Report:\n",classification_report(y_test,y_pred_tree))
